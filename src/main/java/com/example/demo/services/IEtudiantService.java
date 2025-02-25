432d682b662996006976a40e9ae883320308a62c
elyes2dev <elyes.zoghlami.1@esprit.tn>
2025-02-25 11:17:00 +0100

    association

---
+++ src/main/java/com/example/demo/services/IEtudiantService.java
@@ -0,0 +1,9 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Etudiant;
+
+import java.util.List;
+
+public interface IEtudiantService {
+    public List<Etudiant> findAll();
+}
