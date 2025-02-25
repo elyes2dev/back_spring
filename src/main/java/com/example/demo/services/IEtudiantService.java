69ea6644abec7241e0616e23c7a040a7fd28e87a
elyes2dev <elyes.zoghlami.1@esprit.tn>
2025-02-25 12:18:24 +0100

    service+testpostman

--- src/main/java/com/example/demo/services/IEtudiantService.java
+++ src/main/java/com/example/demo/services/IEtudiantService.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface IEtudiantService {
     public List<Etudiant> findAll();
+    public Etudiant findById(int id);
+    public Etudiant save(Etudiant etudiant);
+    public void deleteById(int id);
+    public Etudiant update(Etudiant etudiant);
 }
